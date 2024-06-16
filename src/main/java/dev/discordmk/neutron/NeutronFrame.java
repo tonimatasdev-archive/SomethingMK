8d051d928f592aa40d204d6eb8b3f459d145f5dd
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-06-16 19:28:33 +0200

    Do the WorkspacePanel and create JDragAndDropPanel

--- src/main/java/dev/discordmk/neutron/NeutronFrame.java
+++ src/main/java/dev/discordmk/neutron/NeutronFrame.java
@@ -1,21 +1,25 @@
 package dev.discordmk.neutron;
 
 import dev.discordmk.neutron.components.NeutronMenuBar;
+import dev.discordmk.neutron.panels.WorkspacePanel;
 import dev.discordmk.neutron.util.AssetGetter;
 
 import javax.swing.*;
 
 public class NeutronFrame extends JFrame {
+    
     public NeutronFrame() {
         super("DiscordMK");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(800, 600);
         setLocationRelativeTo(null);
         
         setJMenuBar(new NeutronMenuBar());
+        
+        add(new WorkspacePanel()); // TODO
 
         ImageIcon icon = AssetGetter.getImageIcon("icon");
         if (icon != null) setIconImage(icon.getImage());
+        setVisible(true);
     }
 }
--- src/main/java/dev/discordmk/neutron/components/NeutronMenuBar.java
+++ src/main/java/dev/discordmk/neutron/components/NeutronMenuBar.java
@@ -5,7 +5,8 @@ import javax.swing.*;
 public class NeutronMenuBar extends JMenuBar {
     public NeutronMenuBar() {
         JMenu fileMenu = new JMenu("File");
-
+        
+        
         JMenuItem openItem = new JMenuItem("Open");
         JMenuItem saveItem = new JMenuItem("Save");
         JMenuItem exitItem = new JMenuItem("Exit");
@@ -14,7 +15,13 @@ public class NeutronMenuBar extends JMenuBar {
         fileMenu.add(saveItem);
         fileMenu.addSeparator();
         fileMenu.add(exitItem);
-
+        
         add(fileMenu);
+
+        add(Box.createHorizontalGlue());
+
+        JButton saveButton = new JButton("Save");
+        
+        add(saveButton);
     }
 }
